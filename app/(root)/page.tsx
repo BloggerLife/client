60d5f1a45cb9668963d457b90e9e49e027a7034b
BloggerLife <macarnoldnenyasha@gmail.com>
2024-05-23 13:35:48 +0200

    fab

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,12 +6,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
+      <Image
+        src="/banner.png"
+        alt="banner"
+        width={2000}
+        height={1000}
+        className="w-screen"
+      />
       <Collections />
       <ProductList />
+      <span className="fabContainer">
+        <a href={`https://wa.me/263783536329`}>
+          <Image
+            src="/app.png"
+            alt="whatsapp"
+            width={2000}
+            height={1000}
+            className="fab"
+          />
+        </a>
+      </span>
     </>
   );
 }
 
 export const dynamic = "force-dynamic";
-
